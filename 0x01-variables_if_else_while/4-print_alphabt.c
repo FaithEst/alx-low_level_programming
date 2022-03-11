81e5792138972441570beda3963366f3f3b42cf2
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-11 22:14:36 +0300

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabets = 'a';
+while (alphabets < 'z')
+{
+if (alphabets != 'q' && alphabets != 'e')
+{
+putchar(alphabets);
+}
+putchar('\n');
+}
+return (0);
+}
