b8e2177bbde9c7915d46bd6cc4e120f623d7c4ee
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-15 23:44:56 +0300

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,14 +13,14 @@ long j = 1, k = 2;
 while (i < 50)
 {
 if (i == 0)
-    printf("%ld", j);
+{printf("%ld", j); }
 else if (i == 1)
-    printf(", %ld", k);
+{printf(", %ld", k); }
 else
 {
-    k += j;
-    j = k - j;
-    printf(", %ld", k);
+k += j;
+j = k - j;
+printf(", %ld", k);
 }
 ++i;
 }
