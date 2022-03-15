8de4305e4ce570d73147ca9aecf795d3a9d809f5
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-15 22:24:03 +0300

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,57 @@
+/*
+ * File: 9-times_table.c
+ * Auth: Brennan D Baraban
+ *
+ */
+#include "main.h"
+
+/**
+ * times_table  -  prints the 9 times table, starting with 0
+ * starting from 00:00 to 23:59.
+ * Return: void
+ */
+void times_table(void)
+{
+int i, j, k;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = j * i;
+if (j == 0)
+{
+_putchar(k + '0');
+}
+
+if (k < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+} else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
+// int row = 0, col = 0, product = 0;
+// while (row < 10)
+// {
+//     while (col < 10)
+//     {
+//         product = row * col;
+//         _putchar(product);
+//         _putchar(',');
+//         _putchar(' ');
+//     }
+//     _putchar('\n');
+//     col++;
+// }
+// row++;
+}
