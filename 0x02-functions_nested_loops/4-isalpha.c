2704075cc014256f1b32c164be34c9374317d284
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-15 20:43:57 +0300

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: Brennan D Baraban
+ *
+ */
+#include "main.h"
+
+/**
+ * _islower  - checks for alphabetic character.
+ * @c: checks the character
+ * Return: 1 if character is lowercase,uppercase or a letter
+ * 0 otherwise.
+ */
+int _isalpha(int c)
+{
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    else if (c >= 'A' && c <= 'Z')
+        return (1);
+    else
+        return (0);
+return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
 2. 10 x alphabet
 3. islower
+4. isalpha
