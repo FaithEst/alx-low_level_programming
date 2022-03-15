15d8e98b7487c851ed8145521cb803d643b22687
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-15 19:23:38 +0300

    task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,8 @@
+/*
+ * File: 0-main.c
+ * Auth: Brennan D Baraban
+ *
+ */
 #include "main.h"
 
 
@@ -8,15 +13,15 @@
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-    return (0);
+return (0);
 }
