47b03f100306d4c0ea27b77f9fadfabd5e08a26f
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-15 20:33:52 +0300

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,17 +7,16 @@
 
 /**
  * _islower  - checks for lowercase character.
- * 
  * Return: Always 0.
  */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-    return (1);
+return (1);
 }
 else
 {
-    return (0);
+return (0);
 }
 }
