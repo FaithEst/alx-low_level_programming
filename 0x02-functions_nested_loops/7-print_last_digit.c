fb3a1fc5fd13c62932393569e6872659982ddc81
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-15 21:19:31 +0300

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Brennan D Baraban
+ *
+ */
+#include "main.h"
+
+/**
+ * print_last_digit  -  prints the last digit of a number
+ * @n: checks the number
+ * Return: The value of the last digit
+ */
+int print_last_digit(int n)
+{
+int a;
+
+if (n < 0)
+    n = -n;
+
+a = n % 10;
+
+if (a < 0)
+    a = -a;
+
+_putchar(a + '0');
+
+return (a);
+}
