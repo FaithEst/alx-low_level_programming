f328a5adc9e80790bf85af967935ec72de927463
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-15 22:54:08 +0300

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,7 @@
  */
 void print_to_98(int n)
 {
-while (n <= 0 && n <= 98)
-{
-    _putchar(n);
-    _putchar(',');
-    _putchar(' ');
-}
-n++;
-while (n >= 0 && n <= 98)
+while ( n <= 98)
 {
     _putchar(n);
     _putchar(',');
