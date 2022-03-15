106fbac5afdcbdc5fd80d11a332aab873f686259
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-15 22:30:51 +0300

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+ * File: 10-add.c
+ * Auth: Brennan D Baraban
+ *
+ */
+#include "main.h"
+
+/**
+ * add  -  adds two integers
+ * @i, @j - check the numbers
+ * Return: returns the result.
+ */
+int add(int i, int j)
+{
+int sum = i + j;
+return sum;
+}
