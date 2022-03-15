f7e0805a8f7d8cd376af3a4cc8b627c298e463d8
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-15 21:11:25 +0300

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/*
+ * File: 6-abs.c
+ * Auth: Brennan D Baraban
+ *
+ */
+#include "main.h"
+
+/**
+ * _abs  - computes the absolute value of an integer.
+ * @n: checks the number
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{return n; }
+else
+{return n; }
+}
