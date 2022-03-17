17c663959943117cb4677b151de588790f42f0a1
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-17 20:21:11 +0300

    tasks 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,17 @@
  */
 void more_numbers(void)
 {
-int count = 0, num;
+int count, num;
+count = 0;
 while (count < 10)
 {
 for (num = 0; num <= 14; num++)
 {
 if (num >= 10)
 {
-_putchar(num/10 + '0');
+_putchar(num / 10 + '0');
 }
-_putchar(num + '0');
+_putchar(num % 10 + '0');
 }
 _putchar('\n');
 count++;
