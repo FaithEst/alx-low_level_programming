86e318d0499a8268fb8833feb87804aed813990d
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-17 20:41:38 +0300

    tasks 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,17 @@
  */
 void print_line(int n)
 {
-int i;
 if (n <= 0)
-{_putchar('\n'); }
-else
 {
+_putchar('\n');
+} else
+{
+int i;
+
 for (i = 1; i <= n; i++)
-{_putchar('_'); }
+{
+_putchar('_');
+}
 _putchar('\n');
 }
 }
