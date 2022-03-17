3e5f7b2f5d1b317ca1109fda43f98d9dcc98e852
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-18 00:09:57 +0300

    tasks 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,24 +7,26 @@
  */
 void print_triangle(int size)
 {
-int i, j;
-if (size <= 0)
-{
-_putchar('\n'); 
-}
-else
-{
-for (i = 1; i <= size; i++)
-{
-for (j = i; j < size; j++)
-{
-_putchar(' ');
-}
-for (j = 1; j <= i; j++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
 }
