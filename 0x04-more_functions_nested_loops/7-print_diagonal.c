8843b8703a6c5ca41db880361891ba354c298f9c
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-17 22:27:18 +0300

    tasks 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,6 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: checks he number of times the character \ should be printed
- * Return: void
  */
 void print_diagonal(int n)
 {
