f03bdc4169933980a71544fb5b5cc71caaa26bce
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-17 22:23:39 +0300

    tasks 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,21 +11,21 @@
  */
 void print_diagonal(int n)
 {
-int i,j;
-if (n <= 0)
-{_putchar('\n'); }
-else
-{
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < n; j++)
-{
-if (j == i)
-{_putchar('\\'); }
-else if (j < i)
-{_putchar(' '); }
-}
-_putchar('\n');
-}
-}    
+    if (n <= 0)
+    {_putchar('\n'); }
+    else
+    {
+        int i,j;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (j == i)
+                {_putchar('\\'); }
+                else if (j < i)
+                {_putchar(' '); }
+            }
+            _putchar('\n');
+        }
+    }    
 }
