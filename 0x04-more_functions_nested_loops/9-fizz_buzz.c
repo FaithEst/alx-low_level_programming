e1fbc2b4d4dc2b046cfc55e982b1e7ecd5fbf020
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-17 23:16:11 +0300

    tasks 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ * followed by a new line.
+ */
+int main(void)
+{
+int num;
+for (num = 1; num <= 100; num++)
+{
+if (num % 3 == 0 && num % 5 == 0)
+{printf("FizzBuzz\n"); }
+else if (num % 3 == 0)
+{printf("Fizz\n"); }
+else if (num % 5 == 0)
+{printf("Buzz\n"); }
+}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -8,3 +8,4 @@
 6. The shortest distance between two points is a straight line
 7. I feel like I am diagonally parked in a parallel universe
 8. You are so much sunshine in every square inch
+9. Fizz-Buzz
