fbcd40f18f2fda8810a33746fb75e762ebb8511c
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-17 22:53:53 +0300

    tasks 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,25 +3,25 @@
 /**
  * print_square - prints a square
  * followed by a new line.
- * @size: checks he number of times the character \ should be printed
+ * @size: checks the number of times the character \ should be printed
  */
 void print_square(int size)
 {
-    int a,b;
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (a = 1; a <= size; a++)
-        {
-            for (b = 1; b <= size; b++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
+int a,b;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 1; a <= size; a++)
+{
+for (b = 1; b <= size; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
     
 }
