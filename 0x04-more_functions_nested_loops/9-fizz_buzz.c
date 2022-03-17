0b1b9ea94421d998f5a9f8fb70db82399d6802f6
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-17 23:31:23 +0300

    tasks 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,14 @@ int num;
 for (num = 1; num <= 100; num++)
 {
 if (num % 3 == 0 && num % 5 == 0)
-{printf("FizzBuzz\n"); }
+{printf("FizzBuzz"); }
 else if (num % 3 == 0)
-{printf("Fizz\n"); }
+{printf("Fizz"); }
 else if (num % 5 == 0)
-{printf("Buzz\n"); }
+{printf("Buzz"); }
 else
-{printf("%d\n", num); }
+{printf("%d", num); }
 }
+printf("\n");
 return (0);
 }
