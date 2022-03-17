8128677a9daedefeb7ef3f5ad36d97ef93c0c31f
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-17 20:28:45 +0300

    tasks 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line -  draws a straight line in the terminal.
+ * @n checks the integer
+ * Return: void
+ */
+void print_line(int n)
+{
+for (n = 0; n > 0; n++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -5,3 +5,4 @@
 4. I believe in numbers and signs
 5. Numbers constitute the only universal language
 6. The shortest distance between two points is a straight line
+6. The shortest distance between two points is a straight line
