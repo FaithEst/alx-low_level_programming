ecbf73f351f6d2ed8a80a64eb53737a4a748e0f3
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-21 16:44:24 +0300

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,11 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter and
  * updates the value it points to to 98
+ * @n: pointer to the variable to update
  * Return: void.
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
-    return;
+        *n = 98;
+        return;
 }
