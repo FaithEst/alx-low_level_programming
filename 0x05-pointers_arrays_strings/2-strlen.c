409356ac6f731492a5d12b956123300032db04db
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-21 17:11:34 +0300

    task 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to the check
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
+int _strlen(char *s);
 
 
 #endif /* MAIN_H */
