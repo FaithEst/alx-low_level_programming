288987e5db97c510f84ded2a9924527456f0ab8f
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-21 17:20:02 +0300

    task 2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _puts - prints a string
+ * followed by a new line, to stdout
+ * @str: string to the check
+ * Return: void.
+ */
+void _puts(char *str)
+{
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 int _strlen(char *s);
+void _puts(char *str);
 
 
 #endif /* MAIN_H */
