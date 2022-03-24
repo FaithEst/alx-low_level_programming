cf7e4855d8ef0d8a97f21c393108b08c0b4047f6
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-24 13:45:30 +0300

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,12 @@
 char *_strcat(char *dest, char *src)
 {
     int len = 0, i;
-    
+
     while (dest[len] != '\0')
     {
         len++;
     }
-     
+  
     for (i = 0; src[i] != '\0'; i++)
     {
          dest[len] = src[i];
