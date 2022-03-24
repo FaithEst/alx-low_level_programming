1fdec008792d9c24ad2079538c62ee7bcdec6e46
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-24 22:44:30 +0300

    task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,7 @@ char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
 int _putchar(char c);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* MAIN_H */
