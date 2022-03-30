afcfe62c61aa929506db2d8358c9876e5913f7ef
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-30 19:04:59 +0300

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: the integer to be checked
+ * Return: factorial.
+ */
+int factorial(int n)
+{
+if (n <= 1)
+{
+return (1);
+}
+return (n * factorial(n -1));
+}
