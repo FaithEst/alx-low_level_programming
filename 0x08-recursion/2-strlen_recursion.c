0d78fbf1eeb828c5d0df929066deb2c8c52a32b2
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-30 18:58:32 +0300

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,14 +5,14 @@
  * @s: the string to be checked
  * Return: length of a string.
  */
-int _strlen_recursion(char *s)  
+int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-return 0;
+return (0);
 }
 else
 {
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
 }
