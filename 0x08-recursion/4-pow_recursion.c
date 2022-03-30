598b921f394eb5d61e2196d1acf5414fbb2e4685
Faith Mutitu <mutitufaith53@gmail.com>
2022-03-30 21:01:59 +0300

    description

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a given number.
- * @n: the integer to be checked
- * Return: factorial.
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: 1st the integer
+ * @y: 2nd the integer
+ * Return: value of x
  */
 int _pow_recursion(int x, int y)
 {
