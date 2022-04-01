bbc5532b65a949419d817986c586362fbac810f8
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-01 15:45:58 +0300

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints its name, followed by a new line.
+ * main - prints the number of arguments passed into it
  * @argc: argument counter
  * @argv: argument vector
  * Return: Always 0
