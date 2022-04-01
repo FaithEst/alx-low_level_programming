b63780a7cfdb7db5b4add46e51edc7fcce29dc78
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-01 15:42:04 +0300

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+
+printf("%d", argc);
+return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 0. It ain't what they call you, it's what you answer to
+1. Silence is argument carried out by other means
