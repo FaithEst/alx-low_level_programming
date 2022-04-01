c2120cfaac19e66595cdbbf00b4dcf401d7d2038
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-01 15:34:45 +0300

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - prints its name, followed by a new line.
  * @argc: argument counter
  * @argv: argument vector
- * Return: 0
+ * Return: Always 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 (void)argc;
 
