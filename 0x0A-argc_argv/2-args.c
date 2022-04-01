9d24b96b8e16f52dd5923bba51bc8a77f071f79f
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-01 15:58:20 +0300

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s", argv[i]);
+}
+return (0);
+}
