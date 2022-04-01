f0b2a418371c9e93e3296e806889d79b7f76c1f0
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-01 15:26:46 +0300

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,9 @@
 
 int main (int argc, char *argv[])
 {
-  int i = 0;
-  
-  (void)argc;
-  printf("new name, argv[%d] is %s\n", i, argv[i]);
-  return (0);
+int i = 0;
+(void)argc;
+
+printf("new name, argv[%d] is %s\n", i, argv[i]);
+return (0);
 }
