3b4c6f2e0a1d3f2512050de1d592c5452dcb2a01
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-05 20:56:58 +0300

    task 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,7 +5,6 @@ int actual_prime(int n, int i);
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
- *
  * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+# 0x0B. C - malloc, free
+0. Float like a butterfly, sting like a bee
