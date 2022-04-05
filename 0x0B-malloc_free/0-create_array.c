f281ab29138c8adf98f52c89479ea0fd0b0df208
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-05 21:01:23 +0300

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,23 +12,26 @@
 char *create_array(unsigned int size, char c)
 {
     char *p;
-	unsigned int i = 0;
+    unsigned int i = 0;
 
-	if (size == 0)
-		return (NULL);
+    if (size == 0)
+    {
+        return (NULL);
+    }
 
-	p = (char *) malloc(sizeof(char) * size);
+    p = (char *) malloc(sizeof(char) * size);
 
-	if (p == NULL)
-		return (0);
+    if (p == NULL)
+    {
+        return (0);
+    }
 
-	while (i < size)
-	{
-		*(p + i) = c;
-		i++;
-	}
+    while (i < size)
+    {
+        *(p + i) = c;
+        i++;
+    }
 
-	*(p + i) = '\0';
-
-	return (p);
+    *(p + i) = '\0';
+    return (p);
 }
