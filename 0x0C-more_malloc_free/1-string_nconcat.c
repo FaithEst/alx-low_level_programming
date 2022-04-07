059a2769caafcfe68fac4a013ba212cbe6cbbcdb
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-07 17:29:15 +0300

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
 #include "main.h"
 
+int _strlen(char *s);
 /**
  * string_nconcat - concatenates two strings.
  * @n: number of bytes to concatenate from s2
@@ -11,44 +11,43 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len1;
-	int len2;
-	int i = 0;
-	int j = 0;
-	char *ptr;
+	char *p;
+	int i, c, strlen1;
 
-	for (len1 = 0;s1 && s1[len1]; len1++){}
-	for (len2 = 0;s2 && s2[len2]; len2++){}
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
 
-	if (n < len2)
-	{
-		ptr = malloc(sizeof(char) * (len1 + n + 1));
-	}
-	else
-	{
-		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-	}
+	strlen1 = (unsigned int)_strlen(s1);
 
-	if (!ptr)
-	{
+	p = malloc((strlen1 + n + 1) * sizeof(char));
+	if (p == NULL)
 		return (NULL);
-	}
 
-	while (i < len1)
+	for (i = 0; c = 0; i < (strlen1 + n); i++)
 	{
-		ptr[i] = s1[i];
-		i++;
+		if (i < strlen1)
+			p[i] = s1[i];	/* copy s1[i] to mem area pointed to by p */
+		else
+			p[i] = s2[c++];
 	}
-	while (n < len2 && i < len1 + n)
-	{
-		ptr[i++] = s2[j++];
-	}
-	while (n >= len2 && i < (len1 + len2))
-	{
-		ptr[i++] = s2[j++];
-	}
+	p[i] = '\0';
 
-	ptr[i] = '\0';
+	return (p);
+}
 
-	return (ptr);
+/**
+ * _strlen - returns the length of a str
+ * @s: str
+ * Return: length of str
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
 }
