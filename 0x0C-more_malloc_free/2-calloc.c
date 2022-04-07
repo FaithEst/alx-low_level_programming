988beda7d99891f1ca39949ec4bf705d72e0b8ce
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-07 21:45:09 +0300

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _memset - fills memory with a constant byte
+ * @s: memory area to fill
+ * @b: char to copy
+ * @n: no. of times to copy `b`
+ * Return: pointer to memory area `s`
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
+
+/**
  * *_calloc - allocates memory for an array, using malloc
  * @nmemb: number of elements in the array
  * @size: size of each element member
@@ -20,6 +37,7 @@ if (p == NULL)
     return (NULL);
 }
 
+_memset(p, 0, nmemb * size);	/* sets mem pointed to by p to 0 */
 return (p);
 
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,8 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
+
 
 
 
