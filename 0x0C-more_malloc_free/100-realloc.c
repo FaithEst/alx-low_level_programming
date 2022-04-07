705c1c4cf169968db84b8bc97305620b967235ab
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-07 22:25:33 +0300

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,6 +5,7 @@
  * @ptr: pointer to the memory previously allocated with a call to malloc
  * @old_size: size, in bytes, of the allocated space for ptr
  * @new_size: new size, in bytes of the new memory block
+ * Return: pointer to new memory block, or NULL if error
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -12,7 +13,9 @@ char *p, *copy;
 unsigned int i;
 
 if (new_size == old_size)
-{return (ptr); }
+{
+return (ptr);
+}
 
 if (ptr != NULL && new_size == 0)
 {
@@ -24,17 +27,23 @@ if (ptr == NULL)
 {
 p = malloc(new_size);
 if (p == NULL)
-{return (NULL); }
+{
+return (NULL);
+}
 return (p);
 }
 
 p = malloc(new_size);
 if (p == NULL)
-{return (NULL); }
+{
+return (NULL);
+}
 copy = ptr;
 
 for (i = 0; i < old_size; i++)
-{p[i] = copy[i]; }
+{
+p[i] = copy[i];
+}
 free(ptr);
 
 return (p);
