5efba2f38cb9cf2039fb6512decacc7d9da01dd6
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-07 21:37:07 +0300

    task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,7 +43,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[i++] = s2[j++];
 	}
-
 	ptr[i] = '\0';
 
 	return (ptr);
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
- * @brief 
- * 
+ * *_calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements in the array
+ * @size: size of each element member
+ * Return: pointer to the array
  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *p;
+
+if (nmemb == 0 || size == 0)
+{
+    return (NULL);
+}
+p = malloc(nmemb * size);
+if (p == NULL)
+{
+    return (NULL);
+}
+
+return (p);
+
+}
