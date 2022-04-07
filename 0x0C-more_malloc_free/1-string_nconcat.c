56435f4bf2d16b8f8969e1b3798f6609cc4923f1
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-08 01:13:04 +0300

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,8 +10,10 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
-
-unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+unsigned int i = 0;
+unsigned int j = 0;
+unsigned int len1 = 0;
+unsigned int len2 = 0;
 
 while (s1 && s1[len1])
 {
