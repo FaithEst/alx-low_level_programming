0128d7698f218003641e5396d6c34c4d94f4da40
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-07 14:18:26 +0300

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to calculate
+ * Return pointer if SUCCESSFUL, 98 if FAILURE
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	p = malloc(b);
+
+	if (p == NULL)
+		return (NULL);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+# 0x0C. C - More malloc, free
+0. Trust no one
