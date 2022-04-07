2b7395ae33979bb0a5b03d4000ee300d787e1bfa
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-07 17:12:29 +0300

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,9 +3,9 @@
 
 /**
  * string_nconcat - concatenates two strings.
+ * @n: number of bytes to concatenate from s2
  * @s1: source
  * @s2: destination
- * @n: number of bytes to concatenate from s2
  * 
  * Return - to newly allocated space, if FAILURE null
  */
@@ -17,8 +17,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int j = 0;
 	char *ptr;
 
-	for (len1 = 0; s1[len1] && s1; len1++){}
-	for (len2 = 0; s2[len2] && s2; len2++){}
+	for (len1 = 0;s1 && s1[len1]; len1++){}
+	for (len2 = 0;s2 && s2[len2]; len2++){}
 
 	if (n < len2)
 	{
