4ef871084a87778b6e5487c887d629d4f082cf50
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-07 22:22:55 +0300

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,34 +8,34 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p, *copy;
-	unsigned int i;
+char *p, *copy;
+unsigned int i;
 
-	if (new_size == old_size)
-		return (ptr);
+if (new_size == old_size)
+{return (ptr); }
 
-	if (ptr != NULL && new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
+if (ptr != NULL && new_size == 0)
+{
+free(ptr);
+return (NULL);
+}
 
-	if (ptr == NULL)
-	{
-		p = malloc(new_size);
-		if (p == NULL)
-			return (NULL);
-		return (p);
-	}
+if (ptr == NULL)
+{
+p = malloc(new_size);
+if (p == NULL)
+{return (NULL); }
+return (p);
+}
 
-	p = malloc(new_size);
-	if (p == NULL)
-		return (NULL);
-	copy = ptr;
+p = malloc(new_size);
+if (p == NULL)
+{return (NULL); }
+copy = ptr;
 
-	for (i = 0; i < old_size; i++)
-		p[i] = copy[i];
-	free(ptr);
+for (i = 0; i < old_size; i++)
+{p[i] = copy[i]; }
+free(ptr);
 
-	return (p);
+return (p);
 }
