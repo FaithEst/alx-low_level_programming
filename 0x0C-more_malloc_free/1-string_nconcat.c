874746fb2f0c1b5e9e812ade8a6af8ac9543c592
Faith Mutitu <mutitufaith53@gmail.com>
2022-04-07 16:23:04 +0300

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,47 +11,44 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int len1;
-int len2;
-int i = 0;
-int j = 0;
-char *ptr;
+	int len1;
+	int len2;
+	int i = 0;
+	int j = 0;
+	char *ptr;
 
-for (len1 = 0; s1[len1] && s1; len1++){}
-for (len2 = 0; s2[len2] && s2; len2++){}
+	for (len1 = 0; s1[len1] && s1; len1++){}
+	for (len2 = 0; s2[len2] && s2; len2++){}
 
-if (n < len2)
-{
-	ptr = malloc(sizeof(char) * (len1 + n + 1));
-}
-else
-{
-	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-}
+	if (n < len2)
+	{
+		ptr = malloc(sizeof(char) * (len1 + n + 1));
+	}
+	else
+	{
+		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+	}
 
-if (ptr == NULL)
-{
-	return (NULL);
-}
+	if (!ptr)
+	{
+		return (NULL);
+	}
 
-while (i < len1)
-{
-	ptr[i] = s1[i];
-	i++;
-}
-while (n < len2 && i < len1 + n)
-{
-	ptr[i] = s2[j];
-	i++;
-	j++;
-}
-while (n >= len2 && i < (len1 + len2))
-{
-	ptr[i] = s2[j];
-	i++;
-	j++;
-}
+	while (i < len1)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (n < len2 && i < len1 + n)
+	{
+		ptr[i++] = s2[j++];
+	}
+	while (n >= len2 && i < (len1 + len2))
+	{
+		ptr[i++] = s2[j++];
+	}
 
-ptr[i] = '\0';
-return (ptr);
+	ptr[i] = '\0';
+
+	return (ptr);
 }
